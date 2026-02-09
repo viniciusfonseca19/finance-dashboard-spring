1f79a7312dee0c4411e4199746e809edea05a80e
Vinícius F. <fonsecavinicius117@gmail.com>
2026-02-09 20:29:45 -0300

    Teste de API(FINAL)

--- BACKEND/src/main/java/com/vini/financedashboard/security/SecurityConfig.java
+++ BACKEND/src/main/java/com/vini/financedashboard/security/SecurityConfig.java
@@ -3,15 +3,17 @@ package com.vini.financedashboard.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -24,6 +26,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
-
